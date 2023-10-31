ddfa7c68a6b3f3cd2ffc8f6b53afb1999ca7b542
nanzhiyumu <nanzhiyumu@gmail.com>
2023-10-31 16:27:51 +0800

    微信小程序代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Mapper
 public interface DishMapper {
 
+
     /**
      * 根据分类id查询菜品数量
      * @param categoryId
@@ -38,4 +39,9 @@ public interface DishMapper {
 
     @AutoFill(value = OperationType.UPDATE)
     void update(Dish dish);
+
+    List<Dish> list(Dish dish);
+
+    @Select("select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
+    List<Dish> getBySetmealId(Long setmealId);
 }
